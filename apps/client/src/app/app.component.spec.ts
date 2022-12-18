03a7b2eb354360d2581773755d510c47755efc8a
Jari Moellenbernd <jari.moellenbernd@assecosol.com>
2022-12-18 16:54:03 +0100

    feat: adds basic layout to app.component (#27)

--- apps/client/src/app/app.component.spec.ts
+++ apps/client/src/app/app.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
+
+const CssSelectors = {
+  ToggleNavigationButton: By.css('[data-test-id="navigtationButton"]'),
+  Sidebar: By.css('[data-test-id="sidebar"]'),
+};
 
 describe("AppComponent", () => {
   let fixture: ComponentFixture<AppComponent>;
@@ -8,7 +15,7 @@ describe("AppComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, NoopAnimationsModule],
     }).compileComponents();
   });
 
@@ -23,5 +30,35 @@ describe("AppComponent", () => {
 
   it("should match snapshot", () => {
     expect(fixture).toMatchSnapshot();
+  });
+
+  describe("onToggleNavigation", () => {
+    it("should open the navigation", () => {
+      component.toggleSidebarNavigation = false;
+
+      fixture.debugElement
+        .query(CssSelectors.ToggleNavigationButton)
+        .triggerEventHandler("click", {});
+      fixture.detectChanges();
+
+      expect(
+        fixture.debugElement.query(CssSelectors.Sidebar).componentInstance
+          .opened
+      ).toBe(true);
+    });
+
+    it("should close the navigation", () => {
+      component.toggleSidebarNavigation = true;
+
+      fixture.debugElement
+        .query(CssSelectors.ToggleNavigationButton)
+        .triggerEventHandler("click", {});
+      fixture.detectChanges();
+
+      expect(
+        fixture.debugElement.query(CssSelectors.Sidebar).componentInstance
+          .opened
+      ).toBe(false);
+    });
   });
 });
--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { RouterModule } from "@angular/router";
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+  ],
   selector: "mystack-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "client";
+  title = "My Stack";
+
+  toggleSidebarNavigation = false;
+
+  get isSidebarNavigationOpen(): boolean {
+    return this.toggleSidebarNavigation;
+  }
+
+  onToggleNavigation() {
+    this.toggleSidebarNavigation = !this.toggleSidebarNavigation;
+  }
 }
