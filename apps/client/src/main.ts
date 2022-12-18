63f3efc94ede0d72ce276aa6afbcc139ee65a01f
Jari Moellenbernd <jari.moellenbernd@assecosol.com>
2022-12-18 07:50:10 +0100

    chore: adds angular material to workspace (#27)

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -3,7 +3,9 @@ import { provideRouter } from "@angular/router";
 
 import { AppComponent } from "./app/app.component";
 import { ROUTES } from "./routes";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(ROUTES)],
+  providers: [provideRouter(ROUTES), importProvidersFrom(BrowserAnimationsModule)],
 }).catch((err) => console.error(err));
